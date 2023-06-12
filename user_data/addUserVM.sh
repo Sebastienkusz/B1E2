8855845ef9168cee5c8a392ab435fbe5a099ae3e
Whims-IT <infra@whims-services.com>
2023-06-12 13:41:23 +0200

    [WIP] Rework

--- user_data/addUserVM.sh
+++
@@ -1,3 +0,0 @@
-az vm extension set -n VMAccessForLinux --publisher Microsoft.OSTCExtensions --version 1.4 \
-    --vm-name MyVm --resource-group MyResourceGroup \
-    --protected-settings '{"username":"user1", "ssh_key":"ssh_rsa ..."}'
