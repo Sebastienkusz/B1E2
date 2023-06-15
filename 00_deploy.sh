8df4d0638ca90d5b08cec22d122490a0ef2d76a2
Sebastienkusz <kusz.sebastien@gmail.com>
2023-06-15 16:24:43 +0200

    update deploy

--- 00_deploy.sh
+++ 00_deploy.sh
@@ -5,47 +5,48 @@
 export ResourceGroup="b1e2-gr1"
 export Location="westeurope"
 export Zone="3"
-export PreName="Preproduction-"
+export PreName="preproduction-"
+export Client="esan-"
 
 # Virtual Network
-export VNet=$PreName"Network-Nextcloud"
+export VNet=$PreName"network-nextcloud"
 export AdresseStart="11.0.0.0"
 export NetworkRange="/16"
 
 # Subnet
-export Subnet=$PreName"Subnet-Nextcloud"
+export Subnet=$PreName"subnet-nextcloud"
 export SubnetRange="/29"
 
 # Network Interface Card Variables
-export Nic=$PreName"Nic-Nextcloud"
+export Nic=$PreName"nic-nextcloud"
 
 # Public IP VM Bastion Variables
-export BastionIPName=$PreName"IP-Bastion"
+export BastionIPName=$PreName"ip-bastion"
 
 # Public IP VM Application Variables
-export AppliIPName=$PreName"IP-Nextcloud"
+export AppliIPName=$PreName"ip-nextcloud"
 
 # Label Public IP VM Bastion Variables
-export LabelBastionIPName="esan-preproduction-bastion"
+export LabelBastionIPName=$Client$PreName"bastion"
 
 # Label Public IP VM Application Variables
-export LabelAppliIPName="esan-preproduction-nextcloud"
+export LabelAppliIPName=$Client$PreName"nextcloud"
 
 #Noms des NSG
-export NsgAppliName=$PreName"Nsg-Nextcloud"
-export NsgBastionName=$PreName"Nsg-Bastion"
+export NsgAppliName=$PreName"nsg-nextcloud"
+export NsgBastionName=$PreName"nsg-bastion"
 
 export NsgBastionRuleIPFilter="82.126.234.200"
 export NsgBastionRuleSshPort="10022"
 
 
 #Noms des ressources
-export BastionVMName=$PreName"Vm-Bastion"
-export NextcloudVMName=$PreName"Vm-Nextcloud"
-export BDDName=$PreName"Bdd-Sql"
-export DiskName=$PreName"Disk-Nextcloud"
+export BastionVMName=$PreName"vm-bastion"
+export NextcloudVMName=$PreName"vm-nextcloud"
+export BDDName=$PreName"bdd-sql"
+export DiskName=$PreName"disk-nextcloud"
 
-export ImageOs="Ubuntu2204"
+export ImageOs="UbuntuLTS"
 export BastionVMSize="Standard_B2s"
 export NextcloudVMSize="Standard_D2s_v3"
 
